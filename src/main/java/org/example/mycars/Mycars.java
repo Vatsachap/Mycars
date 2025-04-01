27fa71a2476ab096ee47b26ae7e1a89f13bf922b
vatsachap <shrivatsachapalli@gmail.com>
2025-04-01 18:21:45 -0400

    added all REST API's for the backend system

--- src/main/java/org/example/mycars/Mycars.java
+++ src/main/java/org/example/mycars/Mycars.java
@@ -1,56 +1,37 @@
 package org.example.mycars;
 
-import com.fasterxml.jackson.annotation.JsonTypeId;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "Mycars")
 public class Mycars {
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-    String name;
-    String model;
-    int  power_in_hp;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String model;  // Ensure this is a String
+    private int power_in_hp;
 
-    public Mycars(String model, String name, int power_in_hp) {
-        this.model = model;
+    // Constructors
+    public Mycars() {}
+
+    public Mycars(String name, String model, int power_in_hp) {
         this.name = name;
+        this.model = model;
         this.power_in_hp = power_in_hp;
     }
 
-    public Mycars() {
+    // Getters
+    public String getName() { return name; }
+    public String getModel() { return model; }
+    public int getPower_in_hp() { return power_in_hp; }
 
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String Model() {
-        return model;
-    }
-
-    public int getPower_in_hp() {
-        return power_in_hp;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-    public void setModel(String model) {
-        this.model = model;
-    }
-    public void setPower_in_hp(int power_in_hp) {
-        this.power_in_hp = power_in_hp;
-    }
+    // Setters
+    public void setName(String name) { this.name = name; }
+    public void setModel(String model) { this.model = model; }  // Ensure this setter exists
+    public void setPower_in_hp(int power_in_hp) { this.power_in_hp = power_in_hp; }
 
     @Override
     public String toString() {
-        return "Mycars{" +
-                "name='" + name + '\'' +
-                ", model=" + model +
-                ", power_in_hp=" + power_in_hp +
-                '}';
+        return "Mycars{name='" + name + "', model='" + model + "', power_in_hp=" + power_in_hp + "}";
     }
 }
